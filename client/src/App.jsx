91afd9cfbee553529b776a8bbd22acc372b7a43d
William Hogan <098williamhogan@gmail.com>
2024-08-09 14:12:27 -0600

    fixed theam

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Footer from "./components/Footer/Footer"; // Import the Footer component
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
