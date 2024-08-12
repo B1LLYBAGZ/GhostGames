a7723c7259158e1820bb7fcc07d12d6d3fc5bdcb
MLazalde <mikealazalde@gmail.com>
2024-08-12 12:41:07 -0600

    updated duplicate code in Models

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,5 @@
 const User = require("./User.js");
 const Product = require("./Product.js");
 const Cart = require("./Cart.js");
-const User = require("./User");
 
-module.exports = { User };
+module.exports = { User, Product, Cart };
